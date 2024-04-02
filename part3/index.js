1dff3174ef87fc59b3441150cf9f7cbce3fb688c
r.aishwarya36 <r.aishwarya36@gmail.com>
2024-04-02 21:21:53 +0530

    exercise 3.8

--- part3/index.js
+++ part3/index.js
@@ -3,7 +3,16 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("request-body", function (request, response) {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :request-body"
+  )
+);
 
 let data = [
   {
