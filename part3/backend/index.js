8108d6a6210b16c002fc0c594544f471d7796fe4
r.aishwarya36 <r.aishwarya36@gmail.com>
2024-04-05 23:01:35 +0530

    exercise 3.16

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
-let persons = [];
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -18,10 +18,20 @@ const requestLogger = (request, response, next) => {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
+app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
 };
 
 morgan.token("request-body", function (request, response) {
@@ -57,10 +67,16 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -71,7 +87,10 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
