4943a6a1e0edf726043b463e534025237db680a1
r.aishwarya36 <r.aishwarya36@gmail.com>
2024-04-08 20:35:26 +0530

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  } else {
+    return blogs.reduce((sum, item) => sum + item.likes, 0);
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
