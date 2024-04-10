6ee6bcb10fe882051018fa50f6e7ff426ea54d6f
r.aishwarya36 <r.aishwarya36@gmail.com>
2024-04-10 19:03:10 +0530

    exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -21,9 +21,32 @@ describe("API", () => {
       .get("/api/blogs")
       .expect(200)
       .expect("Content-Type", /application\/json/);
-
     const getBlogsInDb = await helper.blogsInDb();
     assert.strictEqual(getBlogsInDb.length, helper.initialBlogs.length);
+  });
+
+  test("the unique identifier property of the blog posts is named id", async () => {
+    const response = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    const blogs = await response.body;
+
+    //Count the number of blogs with "_id"
+    const _idCount = blogs.reduce(
+      (count, blog) => (blog._id ? count + 1 : count),
+      0
+    );
+
+    //Count the number of blogs with "id"
+    const idCount = blogs.reduce(
+      (count, blog) => (blog.id ? count + 1 : count),
+      0
+    );
+
+    // Check if "_id" count is 0 and "id" count equals the total no. of blogs
+    assert.strictEqual(_idCount, 0);
+    assert.strictEqual(idCount, helper.initialBlogs.length);
   });
 
   after(async () => {
